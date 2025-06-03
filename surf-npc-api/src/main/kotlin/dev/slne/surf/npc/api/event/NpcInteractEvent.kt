ccb79913273db3b40ca9b50219e5ac02bf3b89ca
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-03 20:40:39 +0200

    feat: add SNpc parameter and cancellable functionality to NpcInteractEvent

--- surf-npc-api/src/main/kotlin/dev/slne/surf/npc/api/event/NpcInteractEvent.kt
+++ surf-npc-api/src/main/kotlin/dev/slne/surf/npc/api/event/NpcInteractEvent.kt
@@ -1,12 +1,24 @@
 package dev.slne.surf.npc.api.event
 
+import dev.slne.surf.npc.api.npc.SNpc
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class NpcInteractEvent(): Event() {
+class NpcInteractEvent (
+    val npc: SNpc
+) : Event(), Cancellable{
     private val handlerList = HandlerList()
 
     override fun getHandlers(): HandlerList {
         return handlerList
+    }
+
+    override fun isCancelled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCancelled(cancel: Boolean) {
+        TODO("Not yet implemented")
     }
 }
