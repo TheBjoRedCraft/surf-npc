daf66c6a08c1e3161de56922a957981c692d1137
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-04 16:24:45 +0200

    feat: add NpcDeleteCommand for deleting NPCs with permission checks

---
+++ surf-npc-bukkit/src/main/kotlin/dev/slne/surf/npc/bukkit/command/sub/NpcDeleteCommand.kt
@@ -0,0 +1,38 @@
+package dev.slne.surf.npc.bukkit.command.sub
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+
+import dev.slne.surf.npc.api.npc.SNpc
+import dev.slne.surf.npc.bukkit.command.argument.npcArgument
+import dev.slne.surf.npc.bukkit.util.PermissionRegistry
+import dev.slne.surf.npc.bukkit.util.toPlain
+import dev.slne.surf.npc.core.controller.npcController
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+
+class NpcDeleteCommand(commandName: String) : CommandAPICommand(commandName) {
+    init {
+        withPermission(PermissionRegistry.COMMAND_NPC_DELETE)
+        npcArgument("npc")
+        playerExecutor { player, args ->
+            val npc: SNpc by args
+
+            npc.delete()
+
+            if(npcController.getNpc(npc.id) == null) {
+                player.sendText {
+                    appendPrefix()
+                    success("Der Npc ")
+                    variableValue(npc.data.name.toPlain())
+                    success(" wurde gelöscht.")
+                }
+            } else {
+                player.sendText {
+                    appendPrefix()
+                    error("Der Npc ${npc.data.name.toPlain()} konnte nicht gelöscht werden.")
+                }
+            }
+        }
+    }
+}
