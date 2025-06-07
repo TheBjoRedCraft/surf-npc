2f36f1b2d27c6b99ecd74c822443f66ef07d05c8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-07 12:34:37 +0200

    feat: update build configuration to include required server dependencies for surf-npc-bukkit

--- surf-npc-example/build.gradle.kts
+++ surf-npc-example/build.gradle.kts
@@ -1,12 +1,16 @@
+import dev.slne.surf.surfapi.gradle.util.registerRequired
+
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
 
 dependencies {
     compileOnly(project(":surf-npc-api")) /* Provided by surf-npc-bukkit at runtime */
-    compileOnly(libs.packetevents.spigot)
 }
 surfPaperPluginApi {
     mainClass("dev.slne.surf.npc.example.SurfNpcExamplePlugin")
     authors.add("red")
+    serverDependencies {
+        registerRequired("surf-npc-bukkit")
+    }
 }
