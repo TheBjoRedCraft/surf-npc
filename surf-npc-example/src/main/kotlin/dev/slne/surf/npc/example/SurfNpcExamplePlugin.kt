5459d35cfd463aeb04f8ce245135172baa2480b1
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-06-07 12:34:41 +0200

    feat: add ExampleNpcListener for handling NPC interactions and collisions

--- surf-npc-example/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
+++ surf-npc-example/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
@@ -1,7 +1,32 @@
 package dev.slne.surf.npc.example
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import dev.slne.surf.npc.api.rotation.SNpcRotationType
+import dev.slne.surf.npc.api.skin.SNpcSkinData
+import dev.slne.surf.npc.api.surfNpcApi
+import dev.slne.surf.npc.example.listener.ExampleNpcListener
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Bukkit
 
 class SurfNpcExamplePlugin(): SuspendingJavaPlugin() {
+    override fun onEnable() {
+        Bukkit.getPluginManager().registerEvents(ExampleNpcListener(), this)
 
+        surfNpcApi.createNpc (
+            MiniMessage.miniMessage().deserialize("<rainbow>Example Npc by surf-npc-example"),
+            "example_npc",
+            createSkinData(),
+            surfNpcApi.createLocation(0.0, 0.0, 0.0, "world"),
+            true,
+            SNpcRotationType.PER_PLAYER
+        )
+    }
+
+    fun createSkinData(): SNpcSkinData {
+        return surfNpcApi.createSkinData(
+            owner = "OwnerName",
+            value = "SkinValue",
+            signature = "SkinSignature"
+        )
+    }
 }
---
+++ surf-npc-example/src/main/kotlin/dev/slne/surf/npc/example/listener/ExampleNpcListener.kt
@@ -0,0 +1,30 @@
+package dev.slne.surf.npc.example.listener
+
+import dev.slne.surf.npc.api.event.NpcCollisionEvent
+import dev.slne.surf.npc.api.event.NpcInteractEvent
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+
+class ExampleNpcListener : Listener {
+    @EventHandler
+    fun onNpcCollision(event: NpcCollisionEvent) {
+        val npc = event.npc
+        val player = event.player
+
+        player.velocity = player.location.direction.multiply(-1.5)
+    }
+
+    @EventHandler
+    fun onNpcInteract(event: NpcInteractEvent) {
+        val npc = event.npc
+        val player = event.player
+
+        player.sendText {
+            spacer("[")
+            append(npc.data.displayName)
+            spacer("]")
+            info(" Hey, schön das du da bist!")
+        }
+    }
+}
