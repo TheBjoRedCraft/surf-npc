150bd46ce8084d0f04b87b1be7a0aa6722ef9a0d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-17 18:47:08 +0200

    feat: add build configuration for Surf NPC example plugin

---
+++ surf-npc-example-dsl/build.gradle.kts
@@ -0,0 +1,19 @@
+import dev.slne.surf.surfapi.gradle.util.registerRequired
+
+plugins {
+    id("dev.slne.surf.surfapi.gradle.paper-plugin")
+}
+
+dependencies {
+    compileOnly(project(":surf-npc-api"))
+}
+
+surfPaperPluginApi {
+    mainClass("dev.slne.surf.npc.example.SurfNpcExamplePlugin")
+    authors.add("red")
+    generateLibraryLoader(false)
+    foliaSupported(true)
+    serverDependencies {
+        registerRequired("surf-npc-bukkit")
+    }
+}
