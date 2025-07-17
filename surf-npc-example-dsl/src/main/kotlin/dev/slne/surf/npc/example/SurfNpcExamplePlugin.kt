c85c9e46a8f724d02f5550863cf4eee943c5f926
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-17 19:11:04 +0200

    feat: add detailed comments for NPC creation and property addition in SurfNpcExamplePlugin

--- surf-npc-example-dsl/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
+++ surf-npc-example-dsl/src/main/kotlin/dev/slne/surf/npc/example/SurfNpcExamplePlugin.kt
@@ -16,14 +16,25 @@ class SurfNpcExamplePlugin(): SuspendingJavaPlugin() {
     override fun onEnable() {
         Bukkit.getPluginManager().registerEvents(ExampleNpcListener(), this)
 
+        /**
+         * Creates an example NPC using the DSL provided by the Surf-NPC API.
+         */
         npc {
             displayName = MiniMessage.miniMessage().deserialize("<rainbow>Example Npc by surf-npc-example")
             internalName = "example_npc"
+
+            /**
+             * Skin data can be created using the DSL or by using the SurfNpcApi#getSkin function.
+             */
             skin = dev.slne.surf.npc.api.dsl.skin {
                 ownerName = "CastCrafter"
                 value = "SkinValue"
                 signature = "SkinSignature"
             }
+
+            /**
+             * Location can be created using the DSL.
+             */
             location = dev.slne.surf.npc.api.dsl.location {
                 world = "world"
                 x = 0.0
@@ -34,10 +45,18 @@ class SurfNpcExamplePlugin(): SuspendingJavaPlugin() {
             rotationType = SNpcRotationType.PER_PLAYER
         }
 
+        /**
+         * Retrieves the created NPC using the SurfNpcApi.
+         * If the NPC is not found, it logs a warning message.
+         */
         val npc = surfNpcApi.getNpc("example_npc") ?: return run {
             logger().atWarning().log("Failed to create example NPC: NPC not found after creation.")
         }
 
+        /**
+         * Adds a property to the NPC using the DSL.
+         * This property is of type Boolean, which is registered in the surf-npc Api.
+         */
         surfNpcApi.addProperty(npc, npcProperty {
             key = "example_npc"
             value = true
